528a38b4a6f40ddfe430782798fa7fa12e62af32
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-25 14:35:26 +0530

    Video 23 Page Not Found

---
+++ cartrader/error.vue
@@ -0,0 +1,15 @@
+<script setup>
+const handleError = () => {
+    console.log("here")
+    navigateTo("/")
+}
+</script>
+<template>
+    <div class="flex h-screen justify-center items-center flex-col">
+        <h1 class="text-9xl">
+            404
+        </h1>
+        <p class="mt-7 text-4xl">Page not found</p>
+        <button @click="handleError" class="rounded mt-7 text-2xl bg-blue-400 px-7 text-white">Go back</button>
+    </div>
+</template>
