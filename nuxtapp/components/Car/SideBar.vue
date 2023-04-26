359bbb06f6049fc99df920e98fc2086bb1951161
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-26 09:54:25 +0530

    Video 36 Adding the car Make Filter

--- nuxtapp/components/Car/SideBar.vue
+++ nuxtapp/components/Car/SideBar.vue
@@ -1,4 +1,5 @@
 <script setup>
+const { makes } = useCars();
 const modal = ref({
     make: false,
     location: false,
@@ -20,9 +21,14 @@ const onChangeLocation = () => {
     updateModal("location")
     navigateTo(`/city/${city.value}/car/${route.params.make}`)
 }
+const onChangeMake = (make) => {
+    updateModal('make')
+    navigateTo(`/city/${route.params.city}/car/${make}`)
+}
 </script>
 <template>
     <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+        <!-- Location start -->
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Location</h3>
             <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
@@ -33,11 +39,18 @@ const onChangeLocation = () => {
                 </button>
             </div>
         </div>
+        <!-- Location end -->
 
+        <!-- Make start -->
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Make</h3>
-            <h3 class="text-blue-400 capitalize">Toyota</h3>
+            <h3 class="text-blue-400 capitalize" @click="updateModal('make')">{{ route.params.make || "Any" }}</h3>
+            <div class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex jestify between flex-wrap bg-white"
+                v-if="modal.make">
+                <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">{{ make }}</h4>
+            </div>
         </div>
+        <!-- Make end -->
 
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Price</h3>
