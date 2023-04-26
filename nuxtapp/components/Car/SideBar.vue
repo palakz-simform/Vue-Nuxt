469525a1c4be896eb13ac8d23f42843ba9e914e0
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-26 10:40:48 +0530

    Video 37 Adding the car Price Filter

--- nuxtapp/components/Car/SideBar.vue
+++ nuxtapp/components/Car/SideBar.vue
@@ -6,10 +6,30 @@ const modal = ref({
     price: false
 })
 const city = ref("")
+const priceRange = ref({
+    min: "",
+    max: ""
+})
 const updateModal = (key) => {
     modal.value[key] = !modal.value[key]
 }
 const route = useRoute()
+const router = useRouter()
+
+const priceRangeText = computed(() => {
+    const minPrice = route.query.minPrice
+    const maxPrice = route.query.maxPrice
+    if (!minPrice && !maxPrice) return "Any"
+    else if (!minPrice && maxPrice) {
+        return `<$${maxPrice}`
+    }
+    else if (minPrice && !maxPrice) {
+        return `>$${minPrice}`
+    }
+    else {
+        return `$${minPrice}-$${maxPrice}`
+    }
+})
 const onChangeLocation = () => {
     if (!city.value) return
     if (!isNaN(parseInt(city.value))) {
@@ -24,6 +44,18 @@ const onChangeLocation = () => {
 const onChangeMake = (make) => {
     updateModal('make')
     navigateTo(`/city/${route.params.city}/car/${make}`)
+}
+const onChangePrice = () => {
+    updateModal('price')
+    if (priceRange.value.max && priceRange.value.min) {
+        if (priceRange.value.min > priceRange.value.max) return;
+    }
+    router.push({
+        query: {
+            minPrice: priceRange.value.min,
+            maxPrice: priceRange.value.max
+        }
+    })
 }
 </script>
 <template>
@@ -52,9 +84,16 @@ const onChangeMake = (make) => {
         </div>
         <!-- Make end -->
 
+        <!-- Price start -->
         <div class="p-5 flex justify-between relative cursor-pointer border-b">
             <h3>Price</h3>
-            <h3 class="text-blue-400 capitalize"></h3>
+            <h3 class="text-blue-400 capitalize" @click="updateModal('price')">{{ priceRangeText }}</h3>
+            <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" v-if="modal.price">
+                <input type="number" placeholder="Min" v-model="priceRange.min">
+                <input type="number" placeholder="Max" v-model="priceRange.max">
+                <button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangePrice">Apply</button>
+            </div>
         </div>
+        <!-- Price end -->
     </div>
 </template>
