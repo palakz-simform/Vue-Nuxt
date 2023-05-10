692a66a208c2cc44a6323d79d7e5656942a91273
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-10 12:24:43 +0530

    Video 69 Making POst request from client in create.vue

--- nuxtapp/pages/profile/listings/create.vue
+++ nuxtapp/pages/profile/listings/create.vue
@@ -5,7 +5,7 @@ definePageMeta({
 });
 
 const { makes } = useCars();
-
+const user = useSupabaseUser()
 const info = useState("adInfo", () => {
   return {
     make: "",
@@ -17,9 +17,11 @@ const info = useState("adInfo", () => {
     seats: "",
     features: "",
     description: "",
-    image: null,
+    image: "aaaaaa",
   };
 });
+const errorMessage = ref("")
+
 
 const onChangeInput = (data, name) => {
   info.value[name] = data;
@@ -40,29 +42,69 @@ const inputs = [
   },
   {
     id: 3,
+    title: "Price *",
+    name: "price",
+    placeholder: "10000",
+  },
+  {
+    id: 4,
     title: "Miles *",
     name: "miles",
     placeholder: "10000",
   },
   {
-    id: 4,
+    id: 5,
     title: "City *",
     name: "city",
     placeholder: "Austin",
   },
   {
-    id: 5,
+    id: 6,
     title: "Number of Seats *",
     name: "seats",
     placeholder: "5",
   },
   {
-    id: 6,
+    id: 7,
     title: "Features *",
     name: "features",
     placeholder: "Leather Interior, No Accidents",
   },
 ];
+
+const isButtonDisabled = computed(() => {
+  for (let key in info.value) {
+    if (!info.value[key]) return true
+  };
+  return false
+})
+
+const handleSubmit = async () => {
+  const body = {
+    ...info.value,
+    city: info.value.city.toLowerCase(),
+    features: info.value.features.split(','),
+    numberOfSeats: parseInt(info.value.seats),
+    miles: parseInt(info.value.miles),
+    price: parseInt(info.value.price),
+    year: parseInt(info.value.year),
+    name: `${info.value.make} ${info.value.model}`,
+    listerId: user.value.id,
+    image: "aaaaa"
+  }
+
+  delete body.seats
+
+  try {
+    const response = await $fetch("/api/car/listings", {
+      method: "post",
+      body
+    })
+    navigateTo('/profile/listings')
+  } catch (err) {
+    errorMessage.value = err.statusMessage;
+  }
+}
 </script>
 
 
@@ -77,6 +119,11 @@ const inputs = [
         :placeholder="input.placeholder" @change-input="onChangeInput" />
       <CarAdTextarea title="Description *" name="description" placeholder="" @change-input="onChangeInput" />
       <CarAdImage @change-input="onChangeInput" />
+      <div>
+        <button :disabled="isButtonDisabled" @click="handleSubmit"
+          class="bg-blue-400 text-white rounded py-2 px-7 mt-3">Submit</button>
+        <p v-if="errorMessage" class="mt-3 text-red-400">{{ errorMessage }}</p>
+      </div>
     </div>
   </div>
 </template>
