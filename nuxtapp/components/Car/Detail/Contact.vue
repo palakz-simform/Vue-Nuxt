9a94401ee69dd857d76a93d74bb52ca0eb41d11f
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-10 16:04:18 +0530

    Video 73 Post message endpoint

--- nuxtapp/components/Car/Detail/Contact.vue
+++ nuxtapp/components/Car/Detail/Contact.vue
@@ -1,15 +1,54 @@
+<script setup>
+const message = ref({
+  name: "",
+  email: "",
+  phone: "",
+  message: "",
+})
+
+const errorMessage = ref()
+const successMessage = ref()
+
+const disabledButton = computed(() => {
+  for (let key in message.value) {
+    if (!message.value[key]) return true
+  }
+  return false
+})
+const route = useRoute()
+const onSubmit = async () => {
+  try {
+    const response = await $fetch(`/api/car/listings/${route.params.id}/message`, {
+      method: "post",
+      body: message.value,
+    })
+    message.value = {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    };
+    successMessage.value = "Message Sent!"
+    errorMessage.value = ""
+  } catch (err) {
+    errorMessage.value = err.statusMessage;
+  }
+}
+</script>
 <template>
   <div class="mt-10">
     <div class="flex w-[600px] justify-between">
-      <input type="text" class="border p-1" placeholder="Name" />
-      <input type="text" class="border p-1" placeholder="Email" />
-      <input type="text" class="border p-1" placeholder="Phone" />
+      <input v-model="message.name" type="text" class="border p-1" placeholder="Name" />
+      <input v-model="message.email" type="text" class="border p-1" placeholder="Email" />
+      <input v-model="message.phone" type="text" class="border p-1" placeholder="Phone" />
     </div>
     <div class="flex mt-4 w-[600px]">
-      <textarea class="border p-1 w-full" placeholder="Message"></textarea>
+      <textarea v-model="message.message" class="border p-1 w-full" placeholder="Message"></textarea>
     </div>
-    <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">
+    <button :disabled="disabledButton" class="bg-blue-400 text-white px-10 py-3 rounded mt-4" @click="onSubmit">
       Submit
     </button>
+    <p v-if="errorMessage" class="mt-3 text-red-400">{{ errorMessage }}</p>
+    <p v-if="successMessage" class="mt-3 text-red-400">{{ successMessage }}</p>
   </div>
 </template>
