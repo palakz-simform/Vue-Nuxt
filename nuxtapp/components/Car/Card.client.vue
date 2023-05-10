e5c2b012adcc3e1166fe137a77e2e63e650443c1
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-10 17:49:19 +0530

    Video 76 Section10

--- nuxtapp/components/Car/Card.client.vue
+++ nuxtapp/components/Car/Card.client.vue
@@ -7,6 +7,7 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["favor"]);
+const config = useRuntimeConfig()
 </script>
 
 <template>
@@ -23,7 +24,8 @@ const emit = defineEmits(["favor"]);
     <img class="absolute w-7 right-5 top-2 z-20" :src="favored ? heartFilled : heartOutline" alt=""
       @click="emit('favor', car.id)" />
     <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
-      <NuxtImg :src="car.image" class="w-[300px] h-full" />
+      <NuxtImg :src="`${config.public.supabase.url}/storage/v1/object/public/images/${car.image}`"
+        class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
