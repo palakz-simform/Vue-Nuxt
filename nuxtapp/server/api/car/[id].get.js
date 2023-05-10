3b039ed8ed57898e83c8485e4421836f5b38db08
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-10 11:48:58 +0530

    Video 68 Getting particular car with prisma

--- nuxtapp/server/api/car/[id].get.js
+++ nuxtapp/server/api/car/[id].get.js
@@ -1,8 +1,16 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
 import cars from "@/data/cars.json"
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
     const { id } = event.context.params;
-    const car = cars.find(c => {
-        return c.id === parseInt(id)
+    // const car = cars.find(c => {
+    //     return c.id === parseInt(id)
+    // })
+    const car = await prisma.car.findUnique({
+        where: {
+            id: parseInt(id)
+        }
     })
 
     if (!car) {
