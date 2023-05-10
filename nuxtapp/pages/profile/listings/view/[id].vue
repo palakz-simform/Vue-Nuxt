62a603c7552aa76171138c37eb67aff624f7330a
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-10 17:09:32 +0530

    Video 74

--- nuxtapp/pages/profile/listings/view/[id].vue
+++ nuxtapp/pages/profile/listings/view/[id].vue
@@ -3,13 +3,13 @@ definePageMeta({
   layout: "custom",
 
 });
+
+const route = useRoute()
+const { data: messages } = useFetch(`/api/car/listings/${route.params.id}/message`)
 </script>
 
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
+    <CarMessageCard v-for="message in messages" :key="message.id" :message="message" />
   </div>
 </template>
